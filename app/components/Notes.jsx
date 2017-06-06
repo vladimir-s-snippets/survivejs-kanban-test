e086e31f4f7919ed7927e8cba0661bda95e98acc
Statsenko Vladimir <vovchiksta@gmail.com>
2017-06-06 22:24:16 +0300

    Stilyng

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -3,11 +3,11 @@ import Note from './Note';
 import Editable from './Editable';
 
 export default ({notes, onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}}) => (
-    <ul>{notes.map(({id, editing, task}) =>
+    <ul className="notes">{notes.map(({id, editing, task}) =>
         <li key={id}>
-            <Note onClick={onNoteClick.bind(null, id)}>
+            <Note className="note" onClick={onNoteClick.bind(null, id)}>
                 <Editable editing={editing} value={task} onEdit={onEdit.bind(null, id)} />
-                <button onClick={onDelete.bind(null, id)}>x</button>
+                <button className="delete" onClick={onDelete.bind(null, id)}>x</button>
             </Note>
         </li>
     )}</ul>
