8bf2885eb560a19f7d13ec717a1e43caa05a727d
Statsenko Vladimir <vovchiksta@gmail.com>
2017-06-06 22:03:48 +0300

    Editing notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -37,13 +37,41 @@ export default class App extends React.Component {
         });
     };
 
+    activateNoteEdit = (id) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = true;
+                }
+                return note;
+            })
+        });
+    };
+
+    editNote = (id, task) => {
+        this.setState({
+            note: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = false;
+                    note.task = task;
+                }
+                return note;
+            })
+        });
+    };
+
     render() {
         const {notes} = this.state;
 
         return (
             <div>
                 <button onClick={this.addNote}>+</button>
-                <Notes notes={notes} onDelete={this.deleteNote} />
+                <Notes
+                    notes={notes}
+                    onNoteClick={this.activateNoteEdit}
+                    onEdit={this.editNote}
+                    onDelete={this.deleteNote}
+                />
             </div>
         );
     }
